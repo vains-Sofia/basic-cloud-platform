a08ca388fea55d46b16e1128aba75f1d92422277
yujinxiang <17683906991@163.com>
2024-05-06 10:34:29 +0800

    feat：分离认证服务与资源服务配置

--- basic-services/basic-service-authorization-server/src/main/java/com/basic/cloud/authorization/server/config/AuthorizationServerConfiguration.java
+++ basic-services/basic-service-authorization-server/src/main/java/com/basic/cloud/authorization/server/config/AuthorizationServerConfiguration.java
@@ -3,8 +3,9 @@ package com.basic.cloud.authorization.server.config;
 import com.basic.cloud.oauth2.authorization.property.OAuth2ServerProperties;
 import com.basic.cloud.oauth2.authorization.server.customizer.AuthorizationServerMetadataCustomizer;
 import com.basic.cloud.oauth2.authorization.server.customizer.OidcConfigurerCustomizer;
-import com.basic.cloud.oauth2.authorization.server.email.EmailCaptchaLoginConfigurer;
-import com.basic.cloud.oauth2.authorization.server.handler.authorization.*;
+import com.basic.cloud.oauth2.authorization.server.handler.authorization.ConsentAuthenticationFailureHandler;
+import com.basic.cloud.oauth2.authorization.server.handler.authorization.ConsentAuthorizationResponseHandler;
+import com.basic.cloud.oauth2.authorization.server.handler.authorization.LoginTargetAuthenticationEntryPoint;
 import com.basic.cloud.oauth2.authorization.server.util.OAuth2ConfigurerUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -19,9 +20,6 @@ import org.springframework.security.oauth2.server.authorization.settings.Authori
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
-import java.util.List;
-import java.util.Set;
-
 /**
  * 认证服务过滤器链配置
  *
@@ -30,19 +28,6 @@ import java.util.Set;
 @RequiredArgsConstructor
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfiguration {
-
-    /**
-     * 默认忽略鉴权的地址
-     */
-    private final List<String> DEFAULT_IGNORE_PATHS = List.of(
-            "/login",
-            "/error",
-            "/assets/**",
-            "/favicon.ico",
-            "/login/email",
-            "/swagger-ui/**",
-            "/v3/api-docs/**"
-    );
 
     /**
      * 认证服务配置类
@@ -105,63 +90,6 @@ public class AuthorizationServerConfiguration {
         // Accept access tokens for User Info and/or Client Registration
         http.oauth2ResourceServer((resourceServer) -> resourceServer
                 .jwt(Customizer.withDefaults()));
-        return http.build();
-    }
-
-    /**
-     * 认证与鉴权相关的过滤器链配置
-     *
-     * @param http httpSecurity 实例
-     * @return 认证鉴权相关的过滤器链
-     * @throws Exception 配置错误时抛出
-     */
-    @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-            throws Exception {
-
-        // 禁用 csrf 与 cors
-        http.cors(Customizer.withDefaults());
-        http.csrf(AbstractHttpConfigurer::disable);
-
-        // 合并默认忽略鉴权的地址和配置文件中添加的忽略鉴权的地址
-        Set<String> ignoreUriPaths = oAuth2ServerProperties.getServer().getIgnoreUriPaths();
-        ignoreUriPaths.addAll(DEFAULT_IGNORE_PATHS);
-
-        http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(ignoreUriPaths.toArray(new String[]{})).permitAll()
-                .anyRequest().authenticated()
-        );
-
-        // 自定义认证服务配置文件
-        OAuth2ServerProperties.ServerProperties serverProperties = oAuth2ServerProperties.getServer();
-
-        // Form login handles the redirect to the login page from the
-        // authorization server filter chain
-        http.formLogin(form -> form
-                .loginPage(serverProperties.getLoginPageUri())
-                .loginProcessingUrl(serverProperties.getLoginProcessingUri())
-                .successHandler(new LoginSuccessHandler(serverProperties.getLoginPageUri()))
-                .failureHandler(new LoginFailureHandler(serverProperties.getLoginPageUri()))
-        );
-
-        // 开启oauth2登录支持
-        http.oauth2Login(oauth2Login -> oauth2Login
-                .loginPage(oAuth2ServerProperties.getServer().getLoginPageUri())
-        );
-
-        // 添加BearerTokenAuthenticationFilter，将认证服务当做一个资源服务，解析请求头中的token
-        http.oauth2ResourceServer((resourceServer) -> resourceServer
-                .jwt(Customizer.withDefaults())
-        );
-
-        // 添加邮件登录过滤器
-        http.with(new EmailCaptchaLoginConfigurer<>(), c -> c
-                .loginPage(serverProperties.getLoginPageUri())
-                .loginProcessingUrl(serverProperties.getEmailLoginProcessingUri())
-                .successHandler(new LoginSuccessHandler(serverProperties.getLoginPageUri()))
-                .failureHandler(new LoginFailureHandler(serverProperties.getLoginPageUri()))
-        );
-
         return http.build();
     }
 
---
+++ basic-services/basic-service-authorization-server/src/main/java/com/basic/cloud/authorization/server/config/ResourceServerConfiguration.java
@@ -0,0 +1,102 @@
+package com.basic.cloud.authorization.server.config;
+
+import com.basic.cloud.oauth2.authorization.property.OAuth2ServerProperties;
+import com.basic.cloud.oauth2.authorization.server.email.EmailCaptchaLoginConfigurer;
+import com.basic.cloud.oauth2.authorization.server.handler.authorization.LoginFailureHandler;
+import com.basic.cloud.oauth2.authorization.server.handler.authorization.LoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 资源服务器配置类
+ *
+ * @author Vains
+ */
+@RequiredArgsConstructor
+@Configuration(proxyBeanMethods = false)
+public class ResourceServerConfiguration {
+
+    /**
+     * 默认忽略鉴权的地址
+     */
+    private final List<String> DEFAULT_IGNORE_PATHS = List.of(
+            "/login",
+            "/error",
+            "/assets/**",
+            "/favicon.ico",
+            "/login/email",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    );
+
+    /**
+     * 认证服务配置类
+     */
+    private final OAuth2ServerProperties oAuth2ServerProperties;
+
+    /**
+     * 认证与鉴权相关的过滤器链配置
+     *
+     * @param http httpSecurity 实例
+     * @return 认证鉴权相关的过滤器链
+     * @throws Exception 配置错误时抛出
+     */
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        // 禁用 csrf 与 cors
+        http.cors(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // 合并默认忽略鉴权的地址和配置文件中添加的忽略鉴权的地址
+        Set<String> ignoreUriPaths = oAuth2ServerProperties.getServer().getIgnoreUriPaths();
+        ignoreUriPaths.addAll(DEFAULT_IGNORE_PATHS);
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(ignoreUriPaths.toArray(new String[]{})).permitAll()
+                .anyRequest().authenticated()
+        );
+
+        // 自定义认证服务配置文件
+        OAuth2ServerProperties.ServerProperties serverProperties = oAuth2ServerProperties.getServer();
+
+        // Form login handles the redirect to the login page from the
+        // authorization server filter chain
+        http.formLogin(form -> form
+                .loginPage(serverProperties.getLoginPageUri())
+                .loginProcessingUrl(serverProperties.getLoginProcessingUri())
+                .successHandler(new LoginSuccessHandler(serverProperties.getLoginPageUri()))
+                .failureHandler(new LoginFailureHandler(serverProperties.getLoginPageUri()))
+        );
+
+        // 开启oauth2登录支持
+        http.oauth2Login(oauth2Login -> oauth2Login
+                .loginPage(oAuth2ServerProperties.getServer().getLoginPageUri())
+        );
+
+        // 添加BearerTokenAuthenticationFilter，将认证服务当做一个资源服务，解析请求头中的token
+        http.oauth2ResourceServer((resourceServer) -> resourceServer
+                .jwt(Customizer.withDefaults())
+        );
+
+        // 添加邮件登录过滤器
+        http.with(new EmailCaptchaLoginConfigurer<>(), c -> c
+                .loginPage(serverProperties.getLoginPageUri())
+                .loginProcessingUrl(serverProperties.getEmailLoginProcessingUri())
+                .successHandler(new LoginSuccessHandler(serverProperties.getLoginPageUri()))
+                .failureHandler(new LoginFailureHandler(serverProperties.getLoginPageUri()))
+        );
+
+        return http.build();
+    }
+
+}
