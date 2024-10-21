a98cc1567c2ad1970880300c0e8d3b82fd6f0583
vains-Sofia <17683906991@163.com>
2024-10-21 17:47:01 +0800

    feat：网关添加统一异常处理

---
+++ basic-services/basic-service-gateway/src/main/java/com/basic/cloud/gateway/advice/GatewayExceptionHandlerAdvice.java
@@ -0,0 +1,48 @@
+package com.basic.cloud.gateway.advice;
+
+import com.basic.framework.core.domain.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.support.NotFoundException;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.resource.NoResourceFoundException;
+
+/**
+ * 网关异常处理
+ *
+ * @author vains
+ */
+@Slf4j
+@RestControllerAdvice
+public class GatewayExceptionHandlerAdvice {
+
+    /**
+     * 404 异常处理
+     *
+     * @param response 响应对象
+     * @param ex       具体的异常实例
+     * @return 统一响应类
+     */
+    @ExceptionHandler(NoResourceFoundException.class)
+    public Result<String> exception(ServerHttpResponse response, NoResourceFoundException ex) {
+        log.error(ex.getReason(), ex);
+        response.setStatusCode(ex.getStatusCode());
+        return Result.error(ex.getStatusCode().value(), ex.getReason());
+    }
+
+    /**
+     * 503 服务不可用
+     *
+     * @param response 响应对象
+     * @param ex       具体的异常实例
+     * @return 统一响应类
+     */
+    @ExceptionHandler(NotFoundException.class)
+    public Result<String> exception(ServerHttpResponse response, NotFoundException ex) {
+        log.error(ex.getReason(), ex);
+        response.setStatusCode(ex.getStatusCode());
+        return Result.error(ex.getStatusCode().value(), ex.getReason());
+    }
+
+}
