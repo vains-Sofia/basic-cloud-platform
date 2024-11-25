8f3334d55fab561534edfee987e9944a5e67cff9
vains-Sofia <17683906991@163.com>
2024-11-25 14:41:11 +0800

    test：添加用户FeignClient的Junit测试类

--- basic-services/basic-service-system/src/test/java/com/basic/cloud/system/api/SysBasicUserClientTest.java
+++ basic-services/basic-service-system/src/test/java/com/basic/cloud/system/api/SysBasicUserClientTest.java
@@ -1,8 +1,10 @@
 package com.basic.cloud.system.api;
 
+import com.basic.cloud.system.api.domain.request.FindBasicUserPageRequest;
 import com.basic.cloud.system.api.domain.response.BasicUserResponse;
+import com.basic.cloud.system.api.domain.response.FindBasicUserResponse;
+import com.basic.framework.core.domain.PageResult;
 import com.basic.framework.core.domain.Result;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,14 +20,24 @@ class SysBasicUserClientTest {
     @Autowired
     private SysBasicUserClient basicUserClient;
 
-    @BeforeEach
-    public void before() {
-        System.setProperty("basic.cloud.api.system.path", "/system");
-    }
-
     @Test
     public void getByEmail() {
         Result<BasicUserResponse> responseResult = basicUserClient.getByEmail("17683906991@163.com");
         System.out.println(responseResult);
+    }
+
+    @Test
+    public void findByPage() {
+        FindBasicUserPageRequest request = new FindBasicUserPageRequest();
+        request.setSize(10L);
+        request.setCurrent(1L);
+        Result<PageResult<FindBasicUserResponse>> basicUserByPage = basicUserClient.findByPage(request);
+        System.out.println(basicUserByPage);
+    }
+
+    @Test
+    public void userDetails() {
+        Result<BasicUserResponse> userResponseResult = basicUserClient.userDetails(1L);
+        System.out.println(userResponseResult);
     }
 }
