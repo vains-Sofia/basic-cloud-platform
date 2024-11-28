6412e11a36edcd026aa83d1d7f798541a63e98ae
vains-Sofia <17683906991@163.com>
2024-11-28 10:25:50 +0800

    feat：网关全局异常添加所有其它异常处理

--- basic-services/basic-service-gateway/src/main/java/com/basic/cloud/gateway/advice/GatewayExceptionHandlerAdvice.java
+++ basic-services/basic-service-gateway/src/main/java/com/basic/cloud/gateway/advice/GatewayExceptionHandlerAdvice.java
@@ -3,6 +3,7 @@ package com.basic.cloud.gateway.advice;
 import com.basic.framework.core.domain.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.support.NotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -43,6 +44,18 @@ public class GatewayExceptionHandlerAdvice {
         log.error(ex.getReason(), ex);
         response.setStatusCode(ex.getStatusCode());
         return Result.error(ex.getStatusCode().value(), ex.getReason());
+    }
+
+    /**
+     * 所有其它异常
+     *
+     * @param ex 具体的异常实例
+     * @return 统一响应类
+     */
+    @ExceptionHandler(Exception.class)
+    public Result<String> exception(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        return Result.error(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
     }
 
 }
