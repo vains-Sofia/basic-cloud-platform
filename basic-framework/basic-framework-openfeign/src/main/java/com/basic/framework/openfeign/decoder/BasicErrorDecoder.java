0f4f881b6e8283ea653dfaeb9dcb22474665aa6b
vains-Sofia <17683906991@163.com>
2025-04-21 17:01:35 +0800

    feat：优化OpenFeign失败处理，解析具体的异常响应信息

--- basic-framework/basic-framework-openfeign/src/main/java/com/basic/framework/openfeign/decoder/BasicErrorDecoder.java
+++ basic-framework/basic-framework-openfeign/src/main/java/com/basic/framework/openfeign/decoder/BasicErrorDecoder.java
@@ -1,10 +1,15 @@
 package com.basic.framework.openfeign.decoder;
 
+import com.basic.framework.core.domain.Result;
 import com.basic.framework.core.exception.CloudServiceException;
+import com.basic.framework.core.util.JsonUtils;
 import feign.Response;
+import feign.Util;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * 基础异常解析器
@@ -16,7 +21,23 @@ public class BasicErrorDecoder implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
-        log.warn("OpenFeign调用失败，methodKey：{}, reason：{}", methodKey, response.reason());
+        try {
+            // 读取响应体内容
+            String body = Util.toString(response.body().asReader(StandardCharsets.UTF_8));
+
+            // 尝试解析为 Result 对象
+            Result<Object> result = JsonUtils.toObject(body, Result.class, Object.class);
+            if (result != null) {
+                log.warn("OpenFeign调用失败，methodKey：{}, reason：{}", methodKey, result.getMessage());
+                // 如果解析成功，返回自定义异常
+                return new CloudServiceException(result.getMessage());
+            }
+            log.warn("OpenFeign调用失败，methodKey：{}, reason：{}", methodKey, body);
+            // 如果解析失败，返回默认异常
+            return new CloudServiceException(body);
+        } catch (Exception e) {
+            log.debug("OpenFeign调用失败，methodKey：{}, 读取响应体内容失败，reason：{}", methodKey, e.getMessage());
+        }
         return switch (response.status()) {
             case 400 -> new CloudServiceException(HttpStatus.BAD_REQUEST.getReasonPhrase());
             case 401 -> new CloudServiceException(HttpStatus.UNAUTHORIZED.getReasonPhrase());
