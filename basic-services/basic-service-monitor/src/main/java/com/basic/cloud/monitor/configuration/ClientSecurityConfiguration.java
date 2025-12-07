fda26a7e1ae72ef4f158722dd978285ae65ef65c
vains-Sofia <17683906991@163.com>
2025-12-08 01:00:39 +0800

    fix：去除多余配置

--- basic-services/basic-service-monitor/src/main/java/com/basic/cloud/monitor/configuration/ClientSecurityConfiguration.java
+++ basic-services/basic-service-monitor/src/main/java/com/basic/cloud/monitor/configuration/ClientSecurityConfiguration.java
@@ -96,15 +96,9 @@ public class ClientSecurityConfiguration {
         );
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/*/assets/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(login -> login.loginPage("/oauth2/authorization/messaging-client-oidc"));
-        http.csrf(csrf -> csrf.ignoringRequestMatchers(
-                "/*/assets/**",
-                "/instances",
-                "/actuator/**"
-        ));
         // 添加BearerTokenAuthenticationFilter，解析请求头中的token
         http.oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
 
